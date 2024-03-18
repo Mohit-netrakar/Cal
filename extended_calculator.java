48ccebcaba4017b177f0fa8aad88fd859e88eadd
01fe22bca096 <mohit24122004@gmail.com>
2024-03-18 09:27:53 +0530

    second commit

---
+++ extended_calculator.java
@@ -0,0 +1,11 @@
+public class extended_calculator {
+
+     public float square(float a, float b) {
+        return a * a; // No need for parentheses
+    }
+
+    // Method to perform subtraction
+    public float rectangle(float l, float b) {
+        return l * b; // No need for parentheses
+    }
+}
